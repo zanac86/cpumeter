ac5a1bf488118e3d9c2ba2617330492ee30ca4c6
Belkin Vladimir <zanac86@yandex.ru>
2016-08-15 16:31:22 +0400

    Build with msvc and create nsis installer

--- build_vc2015ce.cmd
+++ build_vc2015ce.cmd
@@ -1,3 +1,16 @@
+@echo off
+
+set BUILD_DIR=release
+
+erase /F /S /Q *.bak *.orig
+rmdir /S /Q %BUILD_DIR%
+mkdir %BUILD_DIR%
+
 call "D:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 rc cpumeter.rc
 cl cpumeter.cpp cpumeter.res /MT /link advapi32.lib user32.lib pdh.lib gdi32.lib
+
+copy cpumeter.exe release\
+erase cpumeter.exe cpumeter.obj
+
+C:\MyPrograms\NSISPortable\App\NSIS\makensis.exe cpumeter.nsi
